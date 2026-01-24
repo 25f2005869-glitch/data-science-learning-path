6eb0e5f86e696ecce15527ad50306eef4955df3a
Saloni tiwari <25f2005869@ds.study.iitm.ac.in>
2026-01-24 11:16:34 +0530

    Add files via upload
    Practiced dictionary with loops and basic result logic

---
+++ day_013.py
@@ -0,0 +1,163 @@
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(student)
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(student["subjects"])
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(student["subjects"]["chem"])
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(student.keys())
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(student.values())
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(list(student.values()))
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(student.items())
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(list(student.items()))
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+pairs = list(student.items())
+print(pairs[0])
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+pairs = list(student.items())
+print(pairs[1])
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(student["name2"])
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+print(student.get("name2"))
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+new_dict = {"city" : "patna"}
+student.update(new_dict)
+
+print(student)
+
+student = {
+    "name" : "rahul kumar",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+student.update({"city" : "patna"})
+
+print(student)
+
+
+
+
