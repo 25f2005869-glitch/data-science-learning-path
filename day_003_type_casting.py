53824d021aba4e971a5320219c9cb506e08b7df3
Saloni tiwari <chnchltiwari@gmail.com>
2026-02-19 10:40:31 +0530

    Add type casting practice for summing two numbers
    Add Day 03 Type Casting practice

---
+++ day_003_type_casting.py
@@ -0,0 +1,12 @@
+# Day 03 - Type Casting Practice
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Convert string to integer
+num1 = int(num1)
+num2 = int(num2)
+
+sum_result = num1 + num2
+
+print("Sum =", sum_result)
